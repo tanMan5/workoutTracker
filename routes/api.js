f59fe6ad8fff40b4a9ffff40ad6193cc44cfdf15
Tanner Nielsen <tan-ali5@outlook.com>
2020-08-01 09:34:15 -0700

    completed routes folder

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,75 @@
 //express.router
+const router = require("express").Router()
+const Workouts = require("../models/workout.js");
+
 
 //posts, puts, gets, and deletes...
+
+router.post("/api/workouts", (req, res) => {
+    console.log(res);
+    Workouts.create({})
+    .then(
+        dbWorkout => {
+            res.json(dbWorkout);
+        }
+    )
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({body, params}, res) => {
+    console.log(res);
+    Workouts.findByIdAndUpdate(
+        params.id,
+        {$push: {exercises: body}},
+        {new: true, runValidators: true}
+    ).then(
+        dbWorkOut => {
+            res.json(dbWorkOut);
+        }
+    )
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+    console.log(res);
+    Workouts.find()
+    .then(
+        dbWorkOut => {
+            res.json(dbWorkOut);
+        }
+    )
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    console.log(res);
+    Workouts.find({})
+    .limit(7)
+    .then(
+        dbWorkOut => {
+            res.json(dbWorkOut);
+        }
+    )
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+    console.log(res);
+    Workouts.findByIdAndDelete(body.id)
+    .then(() => {
+        res.json(true)
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+module.exports = router;
